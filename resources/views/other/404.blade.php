ad2fce904305e1f0b18d523d84cc647ba15db98d
sarna <olsarnat@gmail.com>
2018-02-07 09:39:40 +0200

    work with 404 page

---
+++ resources/views/other/404.blade.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="/css/main.css" />
+
+</head>
+<body>
+<section id="one" class="wrapper style1">
+
+<div class="image fit flush">
+    <img src="/images/pic02.jpg" alt="" />
+</div>
+<header class="special">
+
+    <div id="fof" class="clear">
+        <!-- ####################################################################################################### -->
+        <div class="positioned">
+            <div class="hgroup clear">
+                <h1>404</h1>
+                <h2>Error - Sorry Something Went Wrong !</h2>
+            </div>
+            <p>For Some Reason The Page You Requested Could Not Be Found On Our Server</p>
+        </div>
+        <p class="clear"><a class="go-back" href="javascript:history.go(-1)">&laquo; Go Back</a> <a class="go-home" href="#">Go Home &raquo;</a></p>
+        <!-- ####################################################################################################### -->
+    </div>
+</header>
+<div class="content">
+
+</div>
+</section>
+
+<footer id="footer">
+    <div class="container">
+        <ul class="icons">
+            <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+            <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+            <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+            <li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+        </ul>
+    </div>
+    <div class="copyright">
+        &copy; Untitled. All rights reserved. Images <a href="https://unsplash.com">Unsplash</a> Design <a href="https://templated.co">TEMPLATED</a>
+    </div>
+</footer>
+
+</body>
+</html>
+
+<script src="/resources/assets/js/jquery.min.js"></script>
+<script src="/resources/assets/js/jquery.poptrox.min.js"></script>
+<script src="/resources/assets/js/skel.min.js"></script>
+<script src="/resources/assets/js/util.js"></script>
+<script src="/resources/assets/js/main.js"></script>
+
