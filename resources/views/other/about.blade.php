7d7ab6d7c751c0bbf247b3072a6d74d88a05823b
sarna <olsarnat@gmail.com>
2018-02-08 10:46:06 +0200

    fix admin access and content on user.blade

--- resources/views/other/about.blade.php
+++ resources/views/other/about.blade.php
@@ -1,5 +1,60 @@
 @extends('layouts.app')
 
 @section('content')
+        <!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body>
+<section id="one" class="wrapper style1">
+
+    <div class="image fit flush">
+      <!--  <img src="/images/404.png" alt="" />-->
+    </div>
+    <header class="special">
+
+        <div id="fof" class="clear">
+            <div class="positioned">
+                <div class="hgroup clear">
+                    <h2>Лалала лалал </h2>
+                </div>
+                <p>For Some Reason The Page You Requested Could Not Be Found On Our Server</p>
+            </div>
+            <p class="clear"><a class="go-back" href="javascript:history.go(-1)">&laquo; Go Back</a> <a class="go-home" href="#">Go Home &raquo;</a></p>
+
+        </div>
+    </header>
+</section>
+
+
+
+<footer id="footer">
+    <div class="container">
+        <ul class="icons">
+            <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+            <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+            <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+            <li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+            <li><a href="https://www.linkedin.com/" class="icon fa-linkedin"><span class="label">Linkedin</span> </a> </li>
+            <li><a href="https://github.com/SarnaKhmel" class="icon fa-github"></a><span class="label">GitHub</span> </li>
+        </ul>
+    </div>
+    <div class="copyright">
+        &copy; Untitled. All rights reserved. Images <a href="https://unsplash.com">Unsplash</a> Design <a href="https://templated.co">TEMPLATED</a>
+    </div>
+</footer>
+
+</body>
+</html>
+
+<script src="/resources/assets/js/jquery.min.js"></script>
+<script src="/resources/assets/js/jquery.poptrox.min.js"></script>
+<script src="/resources/assets/js/skel.min.js"></script>
+<script src="/resources/assets/js/util.js"></script>
+<script src="/resources/assets/js/main.js"></script>
 
 @endsection
--- resources/views/userStore/user.blade.php
+++ resources/views/userStore/user.blade.php
@@ -25,75 +25,55 @@
 
                     <!-- One -->
                     <section id="one" class="wrapper style1">
-
-                        <div class="image fit flush">
-                            <img src="/images/pic02.jpg" alt="" />
-                        </div>
-                        <header class="special">
-                            <h2>Fringilla Fermentum Tellus</h2>
-                            <p>vehicula urna sed justo bibendum</p>
-                        </header>
                         <div class="content">
-                            <p>Curabitur eget semper ante. Morbi eleifend ultricies est, a blandit diam vehicula vel. Vestibulum porttitor nisi quis viverra hendrerit. Suspendisse vel volutpat nibh, vel elementum lacus. Maecenas commodo pulvinar dui, at cursus metus imperdiet vel. Vestibulum et efficitur urna. Duis velit nulla, interdum sed felis sit amet, facilisis auctor nunc. Cras luctus urna at risus feugiat scelerisque nec sed diam. </p>
-                            <p>Nunc fringilla metus odio, at rutrum augue tristique vel. Nulla ac tellus a neque ullamcorper porta a vitae ipsum. Morbi est sapien, hendrerit quis mi in, aliquam bibendum orci. Vestibulum imperdiet nibh vitae maximus posuere. Aenean sit amet nibh feugiat, condimentum tellus eu, malesuada nunc. Mauris ac pulvinar turpis, sit amet pharetra est. Ut bibendum justo condimentum, vehicula ex vel, venenatis libero. Etiam vehicula urna sed justo bibendum, ac lacinia nunc pulvinar. Integer nec velit orci. Vestibulum pellentesque varius dapibus. Morbi ullamcorper augue est, sed luctus orci fermentum dictum. Nunc tincidunt, nisl consequat convallis viverra, metus nisl ultricies dui, vitae dapibus ligula urna sit amet nibh. Duis ut venenatis enim.</p>
+
+
+                        <div class="header">
+                            <h1>User information:</h1>
+                        </div>
+                        <div class="content">
+
+                            @foreach($data as $returnUserInfo)
+
+                                <div class="panel panel-body">
+                                    <li id="{{$returnUserInfo["id"]}}" >
+                                            <div class="col-md-offset-1 ">
+                                                <div class="title"><strong>User name: <span class="text">{{$returnUserInfo['name']}}</span></strong></div>
+                                                <div class="description">User email: <span class="text"> {{$returnUserInfo['email']}}</span></div>
+                                                <div class="user">User numder: <span class="text"> {{$returnUserInfo['phone_number']}}</span> </div>
+                                            </div>
+                                    </li>
+
+                                </div>
+
+                            @endforeach
+
+                        </div>
+                        </div>
+                        <div class="content">
+
                         </div>
                     </section>
 
                     <!-- Two -->
                     <section id="two" class="wrapper style2">
                         <header>
-                            <h2>Vestibulum Convallis</h2>
-                            <p>vehicula urna sed justo bibendum</p>
+
+2
                         </header>
                         <div class="content">
                             <div class="gallery">
-                                <div>
-                                    <div class="image fit flush">
-                                        <a href="/images/pic03.jpg"><img src="/images/pic03.jpg" alt="" /></a>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div class="image fit flush">
-                                        <a href="/images/pic01.jpg"><img src="/images/pic01.jpg" alt="" /></a>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div class="image fit flush">
-                                        <a href="/images/pic04.jpg"><img src="/images/pic04.jpg" alt="" /></a>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div class="image fit flush">
-                                        <a href="/images/pic05.jpg"><img src="/images/pic05.jpg" alt="" /></a>
-                                    </div>
-                                </div>
+
                             </div>
                         </div>
                     </section>
 
                     <!-- Three -->
                     <section id="three" class="wrapper">
-                        <div class="spotlight">
-                            <div class="image flush"><img src="/images/pic06.jpg" alt="" /></div>
-                            <div class="inner">
-                                <h3>Viverra Hendrerit</h3>
-                                <p>Curabitur eget semper ante. Morbi eleifend ultricies est, a blandit diam vehicula vel. Vestibulum porttitor nisi quis viverra hendrerit. Suspendisse vel volutpat nibh, vel elementum lacus. Maecenas commodo pulvinar dui, at cursus metus imperdiet vel.</p>
-                            </div>
-                        </div>
-                        <div class="spotlight alt">
-                            <div class="image flush"><img src="/resources/assets/images/pic07.jpg" alt="" /></div>
-                            <div  class="inner">
-                                <h3>Curabitur Eget</h3>
-                                <p>Curabitur eget semper ante. Morbi eleifend ultricies est, a blandit diam vehicula vel. Vestibulum porttitor nisi quis viverra hendrerit. Suspendisse vel volutpat nibh, vel elementum lacus. Maecenas commodo pulvinar dui, at cursus metus imperdiet vel.</p>
-                            </div>
-                        </div>
-                        <div class="spotlight">
-                            <div class="image flush"><img src="/resources/assets/images/pic08.jpg" alt="" /></div>
-                            <div class="inner">
-                                <h3>Morbi Eleifend</h3>
-                                <p>Curabitur eget semper ante. Morbi eleifend ultricies est, a blandit diam vehicula vel. Vestibulum porttitor nisi quis viverra hendrerit. Suspendisse vel volutpat nibh, vel elementum lacus. Maecenas commodo pulvinar dui, at cursus metus imperdiet vel.</p>
-                            </div>
-                        </div>
+
+
+
+
                     </section>
 
                 </div>
